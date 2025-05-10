496c207a98808c78e23d9a3caa91b65bdfe40a5a
Segev Finer <segev208@gmail.com>
2025-05-10 18:48:10 +0300

    WIP

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,18 @@
+// @ts-check
 import js from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.node } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
+  },
+  // @ts-expect-error https://github.com/typescript-eslint/typescript-eslint/issues/10935
   tseslint.configs.recommendedTypeChecked,
   {
     languageOptions: {
@@ -16,4 +22,9 @@ export default defineConfig([
       },
     },
   },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    extends: [eslintPluginPrettierRecommended],
+  },
+  { ignores: ["dist/**"] },
 ]);
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,52 @@
 import http from "node:http";
-import { DispatchFunc, inject } from "light-my-request"
+import { DispatchFunc, inject } from "light-my-request";
 import { defaultFetchMockConfig, FetchMock } from "fetch-mock";
 
 export interface FetchLightMyRequestOptions {
-    /** Optional http server. It is used for binding the `dispatchFunc` */
-    server?: http.Server;
+  /** Optional http server. It is used for binding the `dispatchFunc` */
+  server?: http.Server;
 
-    /** an optional string specifying the client remote address. Defaults to '127.0.0.1' */
-    remoteAddress?: string;
+  /** an optional string specifying the client remote address. Defaults to '127.0.0.1' */
+  remoteAddress?: string;
 }
 
-export function createFetchLightMyRequest(dispatchFunc: DispatchFunc, opts: FetchLightMyRequestOptions = {}) {
-    const fetchMock = new FetchMock({ ...defaultFetchMockConfig });
-    fetchMock.route("*", async (callLog) => {
-        const res = await inject(dispatchFunc, {
-            url: callLog.url,
-            // XXX HTTPMethods is not exported from light-my-request
-            method: callLog.options.method as "GET" | undefined,
-            headers: callLog.options.headers as http.OutgoingHttpHeaders,
-            payload: callLog.options.body ?? undefined,
-            remoteAddress: opts.remoteAddress,
-            server: opts.server,
-            signal: callLog.options.signal ?? undefined,
-        });
-        return new Response(res.body, {
-            status: res.statusCode,
-            statusText: res.statusMessage,
-            headers: res.headers as Record<string, string>,
-        })
+// XXX HTTPMethods is not exported from light-my-request
+type HTTPMethods =
+  | "DELETE"
+  | "delete"
+  | "GET"
+  | "get"
+  | "HEAD"
+  | "head"
+  | "PATCH"
+  | "patch"
+  | "POST"
+  | "post"
+  | "PUT"
+  | "put"
+  | "OPTIONS"
+  | "options";
+
+export function createFetchLightMyRequest(
+  dispatchFunc: DispatchFunc,
+  opts: FetchLightMyRequestOptions = {},
+) {
+  const fetchMock = new FetchMock({ ...defaultFetchMockConfig });
+  fetchMock.route("*", async (callLog) => {
+    const res = await inject(dispatchFunc, {
+      url: callLog.url,
+      method: callLog.options.method as HTTPMethods,
+      headers: callLog.options.headers as http.OutgoingHttpHeaders,
+      payload: callLog.options.body ?? undefined,
+      remoteAddress: opts.remoteAddress,
+      server: opts.server,
+      signal: callLog.options.signal ?? undefined,
     });
-    return fetchMock.fetchHandler.bind(fetchMock);
+    return new Response(res.body, {
+      status: res.statusCode,
+      statusText: res.statusMessage,
+      headers: res.headers as Record<string, string>,
+    });
+  });
+  return fetchMock.fetchHandler.bind(fetchMock);
 }
